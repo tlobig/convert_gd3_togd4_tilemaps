e07ad1310d8e1115a56e047b3c99269717a52753
Thomas Lobig <tom_pdt@gmx.net>
2023-02-17 20:58:30 +0100

    wip, added partial tileset parsing/writing

--- gd_converter.py
+++ gd_converter.py
@@ -1,54 +1,202 @@
-#!/usr/bin/env python
-import os, sys
+#!/usr/bin/env python3
+import os
+import sys
+from collections.abc import Iterable
+
 
 class TileSet:
     def __init__(self) -> None:
+        self.ext_resources = []
+        self.sub_resources = []
+        self.load_steps = 0
+        self.rel_path = ""
+        self.atlasses = []
+        self.colision_shapes = {}
         pass
+
+    def prepare_sub_resources(self, tile_width, tile_height):
+        for sub in self.sub_resources:
+            if sub["type"] == '"ConvexPolygonShape2D"':
+                points = sub["points"]
+                # points are now based around the center
+                points = [points[i] - (tile_width / 2.0 if i % 2 ==
+                                       0 else tile_height / 2.0) for i in range(0, len(points))]
+                self.colision_shapes[sub["id"]] = points
+
 
 class TileMap:
     def __init__(self) -> None:
         pass
 
-def parse_params():
+
+def get_source_and_target_from_params():
     source_dir = ""
     target_dir = ""
     try:
         source_dir = sys.argv[1]
     except:
-        "advisable to add source path as first start parameter, will work from current working directory now"
+        print("\nhint: it is advisable to add source path as first start parameter, will work from current working directory now")
 
-    if source_dir == "": source_dir = os.getcwd()
+    if source_dir == "":
+        source_dir = os.getcwd()
+
     try:
         target_dir = sys.argv[2]
         # if not os.path.exists(target_dir):
     except:
-        "no target dir given"
+        print("\nhint: no target dir given, will add '_godot4' to source dir and use this for output")
+
+    if target_dir == "":
+        target_dir = os.path.join(source_dir + "_godot4")
+
+    print("\n> will parse files in {0}".format(source_dir))
+    print("> will write files to {0}".format(target_dir))
+
     return source_dir, target_dir
 
-def walk_path(root_dir):
+
+def is_valid_target_version(lines: Iterable[str]):
+    if lines[0].find('format=2') == -1:
+        print("Skipping {0}, seems not to be a Godot 3 file".format(
+            source.name))
+        return False
+    else:
+        return True
+
+
+def is_valid_tile_set(lines: Iterable[str]):
+    if lines[0].find('type="TileSet"') == -1:
+        print("Skipping {0}, seems not to be a valid TileSet file".format(
+            source.name))
+        return False
+    return is_valid_target_version(lines)
+
+
+def is_valid_tile_map(lines: Iterable[str]):
+    any_map = False
+    for line in lines:
+        if line.find('type="TileMap"') != -1:
+            any_map = True
+            break
+    if not any_map:
+        print("Skipping {0}, seems not to be a valid TileMap file".format(
+            source.name))
+        return False
+    return is_valid_target_version(lines)
+
+
+def walk_path(root_dir: str):
     tres = []
     tscn = []
-    for root, dirs, files in os.walk(root_dir,topdown=True):
+    for root, dirs, files in os.walk(root_dir, topdown=True):
         dirs[:] = [d for d in dirs if d not in [".git"]]
         for f in files:
             if f.strip().lower().endswith(".tres"):
-                tres.append(os.path.join(root,f))
+                tres.append(os.path.join(root, f))
             elif f.strip().lower().endswith(".tscn"):
-                tscn.append(os.path.join(root,f))
-    if len(tres) + len(tscn) == 0: print("found no files to convert, check current working directory or source path parameter")
-    return([tres,tscn])
+                tscn.append(os.path.join(root, f))
+    if len(tres) + len(tscn) == 0:
+        print("found no files to convert, check current working directory or source path parameter")
+    return ([tres, tscn])
+
+# a dictionary comprehension to disentangle key="value" lines into a dictionary
+
+
+def header_line_to_dict(line: str):
+    line = line.strip()
+    return {l.split("=")[0]: l.split("=")[1] for l in line[line.find(" "):-1].split()}
+
+
+def get_correct_type_name(name: str):
+    if name == '"Texture"':
+        name = '"Texture2D"'
+    return name
+
+
+def str_of_floats_to_list_of_floats(line: str):
+    return [float(s) for s in line.split(",")]
+
+
+def parse_godot3_tile_set(lines: Iterable[str]):
+    tile_set = TileSet()
+    rest_starts_at = 0
+    # we wanna skip ahead during iteration, careful here
+    for index in range(len(lines)):
+        line = lines[index]
+
+        if line.startswith("[ext_resource"):
+            ext = header_line_to_dict(line)
+            ext["type"] = get_correct_type_name(ext["type"])
+            tile_set.ext_resources.append(ext)
+
+        elif line.startswith("[sub_resource"):
+            sub = header_line_to_dict(line)
+            if sub["type"] == '"ConvexPolygonShape2D"':
+                index += 1
+                line = lines[index]
+                sub["points"] = str_of_floats_to_list_of_floats(
+                    line[line.find("(")+1:line.find(")")])
+            print(sub)
+            tile_set.sub_resources.append(sub)
+
+        elif line.startswith("[resource]"):
+            # process the rest differently
+            rest_starts_at = index+1
+            break
+    # second part, parsing tile atlasses
+    # cannot do this here, no indication of 'cell size' without a tilemap - tile_set.prepare_sub_resources(?,?)
+    atlas_id = 0
+    for index in range(rest_starts_at, len(lines)):
+        pass
+    return tile_set
+
+
+def parse_godot3_tile_map(lines: Iterable[str]):
+    tile_map = TileMap()
+    return tile_map
+
+
+def tileset_to_godot_4_tileset(ts: TileSet):
+    lines = []
+    lines.append(
+        '[gd_resource type="TileSet" load_steps={0} format=3]\n\n'.format(ts.load_steps))
+    for ext in ts.ext_resources:
+        lines.append(
+            '[ext_resource type={0} path={1} id={2}]\n\n'.format(ext["type"], ext["path"], ext["id"]))
+        ts.load_steps += 1
+    return lines
+
 
 if __name__ == "__main__":
-    source,target = parse_params()
-    
-    tres,tscn = walk_path(source)
+    source_dir, target_dir = get_source_and_target_from_params()
+
+    tres, tscn = walk_path(source_dir)
+    tilesets, tilemaps = [], []
+
+    print("\nparsing TileSets")
     for t in tres:
-        with open(t,"r") as source:
+        with open(t, "r") as source:
             lines = source.readlines()
-            if lines[0].find('type="TileSet"') == -1:
-                print("Skipping {0}, seems not to be a valid TileSet file".format(source.name))
+            if not is_valid_tile_set(lines):
                 continue
-            elif lines[0].find('format=2') == -1:
-                print("Skipping {0}, seems not to be a Godot 3 TileSet".format(source.name))
+            ts = parse_godot3_tile_set(lines)
+            ts.rel_path = os.path.relpath(t, source_dir)
+            tilesets.append(ts)
+
+    print("\nparsing TileMaps")
+    for t in tscn:
+        with open(t, "r") as source:
+            lines = source.readlines()
+            if not is_valid_tile_map(lines):
                 continue
-            print("processing")
+            tilemaps.append(parse_godot3_tile_map(lines))
+
+    print("\nparsing TileSets")
+    os.makedirs(target_dir, exist_ok=True)
+    for ts in tilesets:
+        lines = tileset_to_godot_4_tileset(ts)
+        target_file_name = os.path.join(target_dir, ts.rel_path)
+        os.makedirs(os.path.dirname(target_file_name), exist_ok=True)
+        with open(target_file_name, mode="w",) as o:
+            print("- writing: " + target_file_name)
+            o.writelines(lines)
